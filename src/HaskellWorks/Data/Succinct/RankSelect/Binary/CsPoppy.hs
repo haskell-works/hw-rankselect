305d539ea80312c3c24900cf27a6d14085be2ffc
John Ky <newhoggy@gmail.com>
2016-07-30 17:32:18 +1000

    CsPoppy construction and rank

--- src/HaskellWorks/Data/Succinct/RankSelect/Binary/CsPoppy.hs
+++ src/HaskellWorks/Data/Succinct/RankSelect/Binary/CsPoppy.hs
@@ -7,12 +7,11 @@ module HaskellWorks.Data.Succinct.RankSelect.Binary.CsPoppy
 import qualified Data.Vector.Storable                                       as DVS
 import           Data.Word
 import           HaskellWorks.Data.Bits.BitRead
+import           HaskellWorks.Data.Bits.BitWise
 import           HaskellWorks.Data.Bits.PopCount.PopCount1
 import           HaskellWorks.Data.Positioning
 import           HaskellWorks.Data.Search
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank0
 import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select0
 import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
 import           HaskellWorks.Data.Vector.VectorLike
 
@@ -29,46 +28,57 @@ popCount1Range start len = popCount1 . DVS.take len . DVS.drop start
 makeCsPoppy :: DVS.Vector Word64 -> CsPoppy
 makeCsPoppy v = CsPoppy
   { csPoppyBits     = v
-  , csPoppy512Index = DVS.constructN (((DVS.length v +          8 - 1) `div`          8) + 1) gen512Index
-  , csPoppyLayer0   = DVS.constructN (((DVS.length v + 0xffffffff - 1) `div` 0xffffffff) + 1) genLayer0
-  , csPoppyLayer1   = DVS.constructN (((DVS.length v +         32 - 1) `div`         32) + 1) genLayer1
+  , csPoppy512Index = DVS.constructN (((DVS.length v +           8 - 1) `div`           8) + 1) gen512Index
+  , csPoppyLayer0   = DVS.constructN (((DVS.length v + 0x100000000 - 1) `div` 0x100000000) + 1) genLayer0
+  , csPoppyLayer1   = DVS.constructN (((DVS.length v +          32 - 1) `div`          32) + 1) genLayer1
   }
-  where gen512Index u = let indexN = DVS.length u - 1 in
+  where csPoppyCum2048  = DVS.constructN (((DVS.length v +          32 - 1) `div`          32) + 1) genCum2048
+        gen512Index u = let indexN = DVS.length u - 1 in
           if indexN == -1
             then 0
-            else getCount (popCount1Range (indexN *          8)          8 v) + DVS.last u
-        genLayer0 u = let indexN = DVS.length u - 1 in
-          if indexN == -1
+            else getCount (popCount1Range (indexN *           8)           8 v) + DVS.last u
+        genCum2048 u = let indexN = DVS.length u in
+          if indexN .&. 0xffffffff == 0
             then 0
-            else getCount (popCount1Range (indexN * 0xffffffff) 0xffffffff v) + DVS.last u
-        genLayer1 u = let indexN = DVS.length u - 1 in
-          if indexN == -1
-            then  0
-            else  let a = getCount (popCount1Range (indexN * 32 +  0)  8 v) + DVS.last u in
-                  let b = getCount (popCount1Range (indexN * 32 +  8)  8 v) + DVS.last u in
-                  let c = getCount (popCount1Range (indexN * 32 + 16)  8 v) + DVS.last u in
-                  let d = getCount (popCount1Range (indexN * 32 + 24)  8 v) + DVS.last u in
-                  a
+            else getCount (popCount1Range ((indexN - 1) *    32)          32 v) + DVS.last u
+        genLayer0 u = let indexN = DVS.length u in
+          if indexN == 0
+            then 0
+            else getCount (popCount1Range (indexN * 0x100000000) 0x100000000 v) + DVS.last u
+        genLayer1 u = let indexN = DVS.length u in
+          let cum = if indexN == 0 -- TODO Check boundary at 4G???
+              then  0
+              else  csPoppyCum2048 !!! fromIntegral indexN in
+          let a = getCount (popCount1Range (indexN * 32 +  0)  8 v) in
+          let b = getCount (popCount1Range (indexN * 32 +  8)  8 v) in
+          let c = getCount (popCount1Range (indexN * 32 + 16)  8 v) in
+          (   ( cum       .&. 0x00000000ffffffff)
+          .|. ((a .<. 32) .&. 0x000003ff00000000)
+          .|. ((b .<. 42) .&. 0x000ffc0000000000)
+          .|. ((c .<. 52) .&. 0x3ff0000000000000)) -- zhou-sea2013 fig 5 (c)
 
 instance BitRead CsPoppy where
   bitRead = fmap makeCsPoppy . bitRead
 
 instance Rank1 CsPoppy where
-  rank1 (CsPoppy v i _ _) p =
-    Count (i !!! toPosition (p `div` 512)) + rank1 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
-
-instance Rank0 CsPoppy where
-  rank0 (CsPoppy v i _ _) p =
-    p `div` 512 * 512 - Count (i !!! toPosition (p `div` 512)) + rank0 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
+  rank1 (CsPoppy v _ layer0 layer1) p = rankPrior + rankInBasicBlock
+    where rankLayer0              = layer0  !!! toPosition (p `div` 0x100000000)
+          rankLayer1Word          = layer1  !!! toPosition (p `div` 2048)
+          rankLayer1A             =  rankLayer1Word .&. 0x00000000ffffffff
+          rankLayer1B             = (rankLayer1Word .&. 0x000003ff00000000) .>. 32
+          rankLayer1C             = (rankLayer1Word .&. 0x000ffc0000000000) .>. 42
+          rankLayer1D             = (rankLayer1Word .&. 0x3ff0000000000000) .>. 52
+          q                       = (p `div` 512) `mod` 4 -- quarter
+          rankLayer1  | q == 0    = rankLayer1A
+                      | q == 1    = rankLayer1A + rankLayer1B
+                      | q == 2    = rankLayer1A + rankLayer1B + rankLayer1C
+                      | q == 3    = rankLayer1A + rankLayer1B + rankLayer1C + rankLayer1D
+                      | otherwise = undefined
+          rankPrior               = Count (rankLayer0 + rankLayer1)
+          rankInBasicBlock        = rank1 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
 
 instance Select1 CsPoppy where
   select1 (CsPoppy v i _ _) p = toCount q * 512 + select1 (DVS.drop (fromIntegral q * 8) v) (p - s)
     where q = binarySearch (fromIntegral p) wordAt 0 (fromIntegral $ DVS.length i - 1)
           s = Count (i !!! q)
           wordAt = (i !!!)
-
-instance Select0 CsPoppy where
-  select0 (CsPoppy v i _ _) p = toCount q * 512 + select0 (DVS.drop (fromIntegral q * 8) v) (p - s)
-    where q = binarySearch (fromIntegral p) wordAt 0 (fromIntegral $ DVS.length i - 1)
-          s = Count (fromIntegral q * 512 - (i !!! q))
-          wordAt o = fromIntegral o * 512 - (i !!! o)
--- test/HaskellWorks/Data/Succinct/RankSelect/Binary/BasicGen.hs
+++ test/HaskellWorks/Data/Succinct/RankSelect/Binary/BasicGen.hs
@@ -3,6 +3,8 @@
 
 module HaskellWorks.Data.Succinct.RankSelect.Binary.BasicGen
   ( genBinaryRankSelectSpec
+  , genBinaryRank0Select0Spec
+  , genBinaryRank1Select1Spec
   ) where
 
 import           Data.Typeable
@@ -17,15 +19,23 @@ import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
 import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1Spec hiding (spec)
 import           Test.Hspec
 
-genBinaryRankSelectSpec :: forall s. (Typeable s, BitRead s, Rank0 s, Rank1 s, Select0 s, Select1 s) => s -> Spec
-genBinaryRankSelectSpec s = describe "Generically" $ do
-  genRank0UpTo8Spec     s
-  genRank0UpTo16Spec    s
+genBinaryRank1Select1Spec :: forall s. (Typeable s, BitRead s, Rank1 s, Select1 s) => s -> Spec
+genBinaryRank1Select1Spec s = describe "For Rank 1" $ do
   genRank1UpTo8Spec     s
   genRank1UpTo16Spec    s
-  genSelect0UpTo8Spec   s
-  genSelect0UpTo16Spec  s
-  genSelect0UpTo32Spec  s
   genSelect1UpTo8Spec   s
   genSelect1UpTo16Spec  s
   genSelect1UpTo32Spec  s
+
+genBinaryRank0Select0Spec :: forall s. (Typeable s, BitRead s, Rank0 s, Select0 s) => s -> Spec
+genBinaryRank0Select0Spec s = describe "For Rank 0" $ do
+  genRank0UpTo8Spec     s
+  genRank0UpTo16Spec    s
+  genSelect0UpTo8Spec   s
+  genSelect0UpTo16Spec  s
+  genSelect0UpTo32Spec  s
+
+genBinaryRankSelectSpec :: forall s. (Typeable s, BitRead s, Rank0 s, Rank1 s, Select0 s, Select1 s) => s -> Spec
+genBinaryRankSelectSpec s = describe "Generically" $ do
+  genBinaryRank1Select1Spec    s
+  genBinaryRank0Select0Spec    s
