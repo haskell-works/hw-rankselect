b0ff0d2b0ce0072ef89ff4bb04f9e7952eb10b12
John Ky <newhoggy@gmail.com>
2016-07-31 00:04:57 +1000

    New sampleRange function

--- src/HaskellWorks/Data/Succinct/RankSelect/Binary/Poppy512S.hs
+++ src/HaskellWorks/Data/Succinct/RankSelect/Binary/Poppy512S.hs
@@ -2,6 +2,7 @@ module HaskellWorks.Data.Succinct.RankSelect.Binary.Poppy512S
     ( Poppy512S(..)
     , Rank1(..)
     , makePoppy512S
+    , sampleRange
     ) where
 
 import qualified Data.Vector.Storable                                       as DVS
@@ -17,7 +18,7 @@ import           HaskellWorks.Data.Vector.VectorLike
 data Poppy512S = Poppy512S
   { csPoppyBits     :: DVS.Vector Word64
   , csPoppy512Index :: DVS.Vector Word64
-  , csPoppyLayerS   :: DVS.Vector Word64 -- Sampling position of each 8192 1-bit
+  , csPoppySamples  :: DVS.Vector Word64 -- Sampling position of each 8192 1-bit
   } deriving (Eq, Show)
 
 popCount1Range :: (DVS.Storable a, PopCount1 a) => Int -> Int -> DVS.Vector a -> Count
@@ -27,7 +28,7 @@ makePoppy512S :: DVS.Vector Word64 -> Poppy512S
 makePoppy512S v = Poppy512S
   { csPoppyBits     = v
   , csPoppy512Index = DVS.constructN (((DVS.length v +           8 - 1) `div`           8) + 1) gen512Index
-  , csPoppyLayerS   = DVS.unfoldrN (fromIntegral (popCount1 v `div` 8192) + 1) genS (0, 0)
+  , csPoppySamples  = DVS.unfoldrN (fromIntegral (popCount1 v `div` 8192) + 1) genS (0, 0)
   }
   where gen512Index u = let indexN = DVS.length u - 1 in
           if indexN == -1
@@ -49,8 +50,17 @@ instance Rank1 Poppy512S where
   rank1 (Poppy512S v i _) p =
     Count (i !!! toPosition (p `div` 512)) + rank1 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
 
+sampleRange :: Poppy512S -> Count -> (Word64, Word64)
+sampleRange (Poppy512S v _ samples) p =
+  let i = (fromIntegral p - 1) `div` 8192 in
+  let pa = samples DVS.! i                  in
+  let pz = samples DVS.! (i + 1)            in
+  if i + 1 < DVS.length samples
+    then (pa, pz)
+    else (pa, fromIntegral (DVS.length v * 64))
+
 instance Select1 Poppy512S where
-  select1 (Poppy512S v i _) p = toCount q * 512 + select1 (DVS.drop (fromIntegral q * 8) v) (p - s)
+  select1 (Poppy512S v i samples) p = toCount q * 512 + select1 (DVS.drop (fromIntegral q * 8) v) (p - s)
     where q = binarySearch (fromIntegral p) wordAt 0 (fromIntegral $ DVS.length i - 1)
           s = Count (i !!! q)
           wordAt = (i !!!)
