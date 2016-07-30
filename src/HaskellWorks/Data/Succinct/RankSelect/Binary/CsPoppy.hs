3a81e0d2169c568a73f4d345037047cadd47bf17
John Ky <newhoggy@gmail.com>
2016-07-30 13:39:08 +1000

    Refactor out new popCount1Range function

--- src/HaskellWorks/Data/Succinct/RankSelect/Binary/CsPoppy.hs
+++ src/HaskellWorks/Data/Succinct/RankSelect/Binary/CsPoppy.hs
@@ -20,42 +20,55 @@ data CsPoppy = CsPoppy
   { csPoppyBits     :: DVS.Vector Word64
   , csPoppy512Index :: DVS.Vector Word64
   , csPoppyLayer0   :: DVS.Vector Word64
+  , csPoppyLayer1   :: DVS.Vector Word64
   } deriving (Eq, Show)
+
+popCount1Range :: (DVS.Storable a, PopCount1 a) => Int -> Int -> DVS.Vector a -> Count
+popCount1Range start len = popCount1 . DVS.take len . DVS.drop start
 
 makeCsPoppy :: DVS.Vector Word64 -> CsPoppy
 makeCsPoppy v = CsPoppy
   { csPoppyBits     = v
   , csPoppy512Index = DVS.constructN (((DVS.length v +          8 - 1) `div`          8) + 1) gen512Index
   , csPoppyLayer0   = DVS.constructN (((DVS.length v + 0xffffffff - 1) `div` 0xffffffff) + 1) genLayer0
+  , csPoppyLayer1   = DVS.constructN (((DVS.length v +         32 - 1) `div`         32) + 1) genLayer1
   }
   where gen512Index u = let indexN = DVS.length u - 1 in
           if indexN == -1
             then 0
-            else getCount (popCount1 (DVS.take 8 (DVS.drop (indexN * 8) v))) + DVS.last u
+            else getCount (popCount1Range (indexN *          8)          8 v) + DVS.last u
         genLayer0 u = let indexN = DVS.length u - 1 in
           if indexN == -1
             then 0
-            else getCount (popCount1 (DVS.take 0xffffffff (DVS.drop (indexN * 0xffffffff) v))) + DVS.last u
+            else getCount (popCount1Range (indexN * 0xffffffff) 0xffffffff v) + DVS.last u
+        genLayer1 u = let indexN = DVS.length u - 1 in
+          if indexN == -1
+            then  0
+            else  let a = getCount (popCount1Range (indexN * 32 +  0)  8 v) + DVS.last u in
+                  let b = getCount (popCount1Range (indexN * 32 +  8)  8 v) + DVS.last u in
+                  let c = getCount (popCount1Range (indexN * 32 + 16)  8 v) + DVS.last u in
+                  let d = getCount (popCount1Range (indexN * 32 + 24)  8 v) + DVS.last u in
+                  a
 
 instance BitRead CsPoppy where
   bitRead = fmap makeCsPoppy . bitRead
 
 instance Rank1 CsPoppy where
-  rank1 (CsPoppy v i _) p =
+  rank1 (CsPoppy v i _ _) p =
     Count (i !!! toPosition (p `div` 512)) + rank1 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
 
 instance Rank0 CsPoppy where
-  rank0 (CsPoppy v i _) p =
+  rank0 (CsPoppy v i _ _) p =
     p `div` 512 * 512 - Count (i !!! toPosition (p `div` 512)) + rank0 (DVS.drop (fromIntegral p `div` 512) v) (p `mod` 512)
 
 instance Select1 CsPoppy where
-  select1 (CsPoppy v i _) p = toCount q * 512 + select1 (DVS.drop (fromIntegral q * 8) v) (p - s)
+  select1 (CsPoppy v i _ _) p = toCount q * 512 + select1 (DVS.drop (fromIntegral q * 8) v) (p - s)
     where q = binarySearch (fromIntegral p) wordAt 0 (fromIntegral $ DVS.length i - 1)
           s = Count (i !!! q)
           wordAt = (i !!!)
 
 instance Select0 CsPoppy where
-  select0 (CsPoppy v i _) p = toCount q * 512 + select0 (DVS.drop (fromIntegral q * 8) v) (p - s)
+  select0 (CsPoppy v i _ _) p = toCount q * 512 + select0 (DVS.drop (fromIntegral q * 8) v) (p - s)
     where q = binarySearch (fromIntegral p) wordAt 0 (fromIntegral $ DVS.length i - 1)
           s = Count (fromIntegral q * 512 - (i !!! q))
           wordAt o = fromIntegral o * 512 - (i !!! o)
