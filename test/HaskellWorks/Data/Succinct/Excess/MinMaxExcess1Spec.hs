d7028a94e2c8af24988639b83d69bada933e3a55
John Ky <newhoggy@gmail.com>
2016-08-05 08:31:29 +1000

    MinMaxExcess1 implementation with tests

--- test/HaskellWorks/Data/Succinct/Excess/MinMaxExcess1Spec.hs
+++ test/HaskellWorks/Data/Succinct/Excess/MinMaxExcess1Spec.hs
@@ -14,23 +14,71 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.Succinct.Excess.MinMaxExcess1Spec" $ do
-  it "Excess should be between min excess and max excess" $
-    forAll (choose (0, 255 :: Word8)) $ \w ->
-      let (minE, e, maxE) = minMaxExcess1 w in
-      minE <= e && e <= maxE
-  it "minE2 == minE0 `min` (minE1 + e0)" $
-    forAll (choose (0, 255 :: Word8)) $ \w0 ->
-      forAll (choose (0, 255 :: Word8)) $ \w1 ->
-        let w2 :: Word16 = leConcat w0 w1 in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
-        minE2 == minE0 `min` (minE1 + e0)
-  it "maxE2 == maxE0 `max` (maxE1 + e0)" $
-    forAll (choose (0, 255 :: Word8)) $ \w0 ->
-      forAll (choose (0, 255 :: Word8)) $ \w1 ->
-        let w2 :: Word16 = leConcat w0 w1 in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
-        maxE2 == maxE0 `max` (maxE1 + e0)
+  describe "For Word8" $ do
+    it "Excess should be between min excess and max excess" $
+      forAll (choose (0, 255 :: Word8)) $ \w ->
+        let (minE, e, maxE) = minMaxExcess1 w in
+        minE <= e && e <= maxE
+    it "minE2 == minE0 `min` (minE1 + e0)" $
+      forAll (choose (0, 255 :: Word8)) $ \w0 ->
+        forAll (choose (0, 255 :: Word8)) $ \w1 ->
+          let w2 :: Word16 = leConcat w0 w1 in
+          let (minE0, e0, _) = minMaxExcess1 w0 in
+          let (minE1, _ , _) = minMaxExcess1 w1 in
+          let (minE2, _ , _) = minMaxExcess1 w2 in
+          minE2 == minE0 `min` (minE1 + e0)
+    it "maxE2 == maxE0 `max` (maxE1 + e0)" $
+      forAll (choose (0, 255 :: Word8)) $ \w0 ->
+        forAll (choose (0, 255 :: Word8)) $ \w1 ->
+          let w2 = leConcat w0 w1 in
+          let (_, e0, maxE0) = minMaxExcess1 w0 in
+          let (_, _ , maxE1) = minMaxExcess1 w1 in
+          let (_, _ , maxE2) = minMaxExcess1 w2 in
+          maxE2 == maxE0 `max` (maxE1 + e0)
+  describe "For Word16" $ do
+    it "Excess should be between min excess and max excess" $
+      forAll (choose (0, 255 :: Word16)) $ \w ->
+        let (minE, e, maxE) = minMaxExcess1 w in
+        minE <= e && e <= maxE
+    it "minE2 == minE0 `min` (minE1 + e0)" $
+      forAll (choose (0, 255 :: Word16)) $ \w0 ->
+        forAll (choose (0, 255 :: Word16)) $ \w1 ->
+          let w2 = leConcat w0 w1 in
+          let (minE0, e0, _) = minMaxExcess1 w0 in
+          let (minE1, _ , _) = minMaxExcess1 w1 in
+          let (minE2, _ , _) = minMaxExcess1 w2 in
+          minE2 == minE0 `min` (minE1 + e0)
+    it "maxE2 == maxE0 `max` (maxE1 + e0)" $
+      forAll (choose (0, 255 :: Word16)) $ \w0 ->
+        forAll (choose (0, 255 :: Word16)) $ \w1 ->
+          let w2 = leConcat w0 w1 in
+          let (_, e0, maxE0) = minMaxExcess1 w0 in
+          let (_, _ , maxE1) = minMaxExcess1 w1 in
+          let (_, _ , maxE2) = minMaxExcess1 w2 in
+          maxE2 == maxE0 `max` (maxE1 + e0)
+    describe "For Word32" $ do
+      it "Excess should be between min excess and max excess" $
+        forAll (choose (0, 255 :: Word32)) $ \w ->
+          let (minE, e, maxE) = minMaxExcess1 w in
+          minE <= e && e <= maxE
+      it "minE2 == minE0 `min` (minE1 + e0)" $
+        forAll (choose (0, 255 :: Word32)) $ \w0 ->
+          forAll (choose (0, 255 :: Word32)) $ \w1 ->
+            let w2 = leConcat w0 w1 in
+            let (minE0, e0, _) = minMaxExcess1 w0 in
+            let (minE1, _ , _) = minMaxExcess1 w1 in
+            let (minE2, _ , _) = minMaxExcess1 w2 in
+            minE2 == minE0 `min` (minE1 + e0)
+      it "maxE2 == maxE0 `max` (maxE1 + e0)" $
+        forAll (choose (0, 255 :: Word32)) $ \w0 ->
+          forAll (choose (0, 255 :: Word32)) $ \w1 ->
+            let w2 = leConcat w0 w1 in
+            let (_, e0, maxE0) = minMaxExcess1 w0 in
+            let (_, _ , maxE1) = minMaxExcess1 w1 in
+            let (_, _ , maxE2) = minMaxExcess1 w2 in
+            maxE2 == maxE0 `max` (maxE1 + e0)
+  describe "For Word64" $ do
+    it "Excess should be between min excess and max excess" $
+      forAll (choose (0, 255 :: Word64)) $ \w ->
+        let (minE, e, maxE) = minMaxExcess1 w in
+        minE <= e && e <= maxE
