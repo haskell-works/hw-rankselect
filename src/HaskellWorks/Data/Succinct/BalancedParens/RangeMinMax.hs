4451652a8d04cdbbfbb7ed3e20c075931630ade7
John Ky <newhoggy@gmail.com>
2016-08-08 08:36:36 +1000

    Fix algorithmic complexity issue due to poor lazy handling

--- src/HaskellWorks/Data/Succinct/BalancedParens/RangeMinMax.hs
+++ src/HaskellWorks/Data/Succinct/BalancedParens/RangeMinMax.hs
@@ -7,7 +7,6 @@ module HaskellWorks.Data.Succinct.BalancedParens.RangeMinMax
   , mkRangeMinMax
   ) where
 
-import           Control.Applicative
 import           Data.Int
 import qualified Data.Vector                                              as DV
 import qualified Data.Vector.Storable                                     as DVS
@@ -34,6 +33,10 @@ data RangeMinMax = RangeMinMax
   , rangeMinMaxL2Excess :: DVS.Vector Int16
   }
 
+wordsL0 :: Int
+wordsL0 = 8
+{-# INLINE wordsL0 #-}
+
 wordsL1 :: Int
 wordsL1 = 8
 {-# INLINE wordsL1 #-}
@@ -53,13 +56,13 @@ mkRangeMinMax bp = RangeMinMax
   { rangeMinMaxBP       = bp
   , rangeMinMaxL0Min    = DVS.constructN lenL0 (\v -> let (minE, _, _) = allMinMaxL0 DV.! DVS.length v in fromIntegral minE)
   , rangeMinMaxL0Max    = DVS.constructN lenL0 (\v -> let (_, _, maxE) = allMinMaxL0 DV.! DVS.length v in fromIntegral maxE)
-  , rangeMinMaxL0Excess = DVS.constructN lenL0 (\v -> let (_, e,    _) = allMinMaxL0 DV.! DVS.length v in fromIntegral e)
+  , rangeMinMaxL0Excess = rangeMinMaxL0ExcessA
   , rangeMinMaxL1Min    = DVS.constructN lenL1 (\v -> let (minE, _, _) = allMinMaxL1 DV.! DVS.length v in fromIntegral minE)
   , rangeMinMaxL1Max    = DVS.constructN lenL1 (\v -> let (_, _, maxE) = allMinMaxL1 DV.! DVS.length v in fromIntegral maxE)
-  , rangeMinMaxL1Excess = DVS.constructN lenL1 (\v -> let (_, e,    _) = allMinMaxL1 DV.! DVS.length v in fromIntegral e)
+  , rangeMinMaxL1Excess = rangeMinMaxL1ExcessA
   , rangeMinMaxL2Min    = DVS.constructN lenL2 (\v -> let (minE, _, _) = allMinMaxL2 DV.! DVS.length v in fromIntegral minE)
   , rangeMinMaxL2Max    = DVS.constructN lenL2 (\v -> let (_, _, maxE) = allMinMaxL2 DV.! DVS.length v in fromIntegral maxE)
-  , rangeMinMaxL2Excess = DVS.constructN lenL2 (\v -> let (_, e,    _) = allMinMaxL2 DV.! DVS.length v in fromIntegral e)
+  , rangeMinMaxL2Excess = rangeMinMaxL2ExcessB
   }
   where lenBP         = fromIntegral (vLength bp) :: Int
         lenL0         = lenBP + 1
@@ -75,6 +78,23 @@ mkRangeMinMax bp = RangeMinMax
         allMinMaxL2   = DV.constructN lenL2 genMinMaxL2
         genMinMaxL2 v = let len = DV.length v in
                         minMaxExcess1 (DVS.take wordsL2 (DVS.drop (fromIntegral len * wordsL2) bp))
+        rangeMinMaxL0ExcessA = DVS.constructN lenL0 (\v -> let (_, e,    _) = allMinMaxL0 DV.! DVS.length v in fromIntegral e) :: DVS.Vector Int8
+        rangeMinMaxL1ExcessA = DVS.constructN lenL1 (\v -> let (_, e,    _) = allMinMaxL1 DV.! DVS.length v in fromIntegral e) :: DVS.Vector Int16
+        -- rangeMinMaxL2ExcessA = DVS.constructN lenL2 (\v -> let (_, e,    _) = allMinMaxL2 DV.! DVS.length v in fromIntegral e) :: DVS.Vector Int16
+        -- rangeMinMaxL1ExcessB = DVS.constructN lenL1 (\v -> DVS.foldr ((+) . fromIntegral) 0 (takeDrop8 bitsRatioL1  (DVS.length v * bitsRatioL1) rangeMinMaxL0ExcessA)) :: DVS.Vector Int16
+        rangeMinMaxL2ExcessB = DVS.constructN lenL2 (\v -> DVS.foldr (+)                  0 (takeDrop16 bitsRatioL2 (DVS.length v * bitsRatioL2) rangeMinMaxL1ExcessA)) :: DVS.Vector Int16
+
+bitsRatioL1 :: Int
+bitsRatioL1 = wordsL1 `div` wordsL0
+
+bitsRatioL2 :: Int
+bitsRatioL2 = wordsL2 `div` wordsL1
+
+takeDrop16 :: Int -> Int -> DVS.Vector Int16 -> DVS.Vector Int16
+takeDrop16 n o v = DVS.take n (DVS.drop o v)
+
+takeDrop8 :: Int -> Int -> DVS.Vector Int8 -> DVS.Vector Int8
+takeDrop8 n o v = DVS.take n (DVS.drop o v)
 
 instance TestBit RangeMinMax where
   (.?.) = (.?.) . rangeMinMaxBP
@@ -92,66 +112,79 @@ instance BitLength RangeMinMax where
   bitLength = bitLength . rangeMinMaxBP
   {-# INLINE bitLength #-}
 
-findCloseN' :: RangeMinMax -> Int -> Count -> Maybe Count
+data Result a = Progress a | NoSkip | Fail
+  deriving (Eq, Show)
+
+(<||>) :: Result a -> Result a -> Result a
+Fail        <||> _          = Fail
+NoSkip      <||> b          = b
+Progress a  <||> _          = Progress a
+
+resultToMaybe :: Result a -> Maybe a
+resultToMaybe Fail          = Nothing
+resultToMaybe NoSkip        = Nothing
+resultToMaybe (Progress a)  = Just a
+
+findCloseN' :: RangeMinMax -> Int -> Count -> Result Count
 findCloseN' v s p = if v `closeAt` p
   then if s <= 1
-    then Just p
+    then Progress p
     else rangeMinMaxFindCloseN v (s - 1) (p + 1)
   else rangeMinMaxFindCloseN v (s + 1) (p + 1)
 {-# INLINE findCloseN' #-}
 
-rangeMinMaxFindCloseN :: RangeMinMax -> Int -> Count -> Maybe Count
-rangeMinMaxFindCloseN v s p  = if 0 < p && p <= bitLength v
+rangeMinMaxFindCloseN :: RangeMinMax -> Int -> Count -> Result Count
+rangeMinMaxFindCloseN v s p = if 0 < p && p <= bitLength v
   then if (p - 1) `mod` elemBitLength (rangeMinMaxBP v) /= 0
     then findCloseN' v s p
     else if (p - 1) `mod` bitsL1 /= 0
-      then rangeMinMaxFindCloseNL0 v s p <|> findCloseN' v s p
+      then rangeMinMaxFindCloseNL0 v s p <||> findCloseN' v s p
       else if (p - 1) `mod` bitsL2 /= 0
-        then rangeMinMaxFindCloseNL1 v s p <|> rangeMinMaxFindCloseNL0 v s p <|> findCloseN' v s p
-        else rangeMinMaxFindCloseNL2 v s p <|> rangeMinMaxFindCloseNL1 v s p <|> rangeMinMaxFindCloseNL0 v s p <|> findCloseN' v s p
-  else Nothing
+        then rangeMinMaxFindCloseNL1 v s p <||> rangeMinMaxFindCloseNL0 v s p <||> findCloseN' v s p
+        else rangeMinMaxFindCloseNL2 v s p <||> rangeMinMaxFindCloseNL1 v s p <||> rangeMinMaxFindCloseNL0 v s p <||> findCloseN' v s p
+  else Fail
 {-# INLINE rangeMinMaxFindCloseN #-}
 
-rangeMinMaxFindCloseNL0 :: RangeMinMax -> Int -> Count -> Maybe Count
-rangeMinMaxFindCloseNL0 v s p  =
+rangeMinMaxFindCloseNL0 :: RangeMinMax -> Int -> Count -> Result Count
+rangeMinMaxFindCloseNL0 v s p =
   let i = (p - 1) `div` elemBitLength bp in
   let minE = fromIntegral (mins !!! fromIntegral i) :: Int in
   if fromIntegral s + minE <= 0
-    then Nothing
+    then NoSkip
     else if v `closeAt` p && s <= 1
-      then Just p
+      then Progress p
       else let excess  = fromIntegral (excesses !!! fromIntegral i)  :: Int in
-            rangeMinMaxFindCloseN v (fromIntegral (excess + fromIntegral s)) (p + 64)
+            rangeMinMaxFindCloseN  v (fromIntegral (excess + fromIntegral s)) (p + 64)
   where bp                    = rangeMinMaxBP v
         mins                  = rangeMinMaxL0Min v
         excesses              = rangeMinMaxL0Excess v
 {-# INLINE rangeMinMaxFindCloseNL0 #-}
 
-rangeMinMaxFindCloseNL1 :: RangeMinMax -> Int -> Count -> Maybe Count
-rangeMinMaxFindCloseNL1 v s p  =
+rangeMinMaxFindCloseNL1 :: RangeMinMax -> Int -> Count -> Result Count
+rangeMinMaxFindCloseNL1 v s p =
   let i = (p - 1) `div` bitsL1 in
   let minE = fromIntegral (mins !!! fromIntegral i) :: Int in
   if fromIntegral s + minE <= 0
-    then  Nothing
+    then  NoSkip
     else if v `closeAt` p && s <= 1
-      then Just p
+      then Progress p
       else let excess  = fromIntegral (excesses !!! fromIntegral i)  :: Int in
-            rangeMinMaxFindCloseN v (fromIntegral (excess + fromIntegral s)) (p + bitsL1)
+            rangeMinMaxFindCloseN  v (fromIntegral (excess + fromIntegral s)) (p + bitsL1)
   where mins                  = rangeMinMaxL1Min v
         excesses              = rangeMinMaxL1Excess v
 {-# INLINE rangeMinMaxFindCloseNL1 #-}
 
 
-rangeMinMaxFindCloseNL2 :: RangeMinMax -> Int -> Count -> Maybe Count
-rangeMinMaxFindCloseNL2 v s p  =
+rangeMinMaxFindCloseNL2 :: RangeMinMax -> Int -> Count -> Result Count
+rangeMinMaxFindCloseNL2 v s p =
   let i = (p - 1) `div` bitsL2 in
   let minE = fromIntegral (mins !!! fromIntegral i) :: Int in
   if fromIntegral s + minE <= 0
-    then  Nothing
+    then  NoSkip
     else if v `closeAt` p && s <= 1
-      then Just p
+      then Progress p
       else let excess  = fromIntegral (excesses !!! fromIntegral i)  :: Int in
-            rangeMinMaxFindCloseN v (fromIntegral (excess + fromIntegral s)) (p + bitsL2)
+            rangeMinMaxFindCloseN  v (fromIntegral (excess + fromIntegral s)) (p + bitsL2)
   where mins                  = rangeMinMaxL2Min v
         excesses              = rangeMinMaxL2Excess v
 {-# INLINE rangeMinMaxFindCloseNL2 #-}
@@ -160,7 +193,7 @@ instance BalancedParens RangeMinMax where
   openAt            = openAt      . rangeMinMaxBP
   closeAt           = closeAt     . rangeMinMaxBP
   -- findOpenN         = findOpenN   . rangeMinMaxBP
-  findCloseN v s    = rangeMinMaxFindCloseN v (fromIntegral s)
+  findCloseN v s p  = resultToMaybe (rangeMinMaxFindCloseN v (fromIntegral s) p)
 
   {-# INLINE openAt      #-}
   {-# INLINE closeAt     #-}
