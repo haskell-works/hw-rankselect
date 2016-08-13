6683ab3c2c0eb6589b310ebcb630a7bea1059722
John Ky <newhoggy@gmail.com>
2016-08-13 14:09:47 +1000

    Inlining

--- src/HaskellWorks/Data/Succinct/Excess/MinMaxExcess1.hs
+++ src/HaskellWorks/Data/Succinct/Excess/MinMaxExcess1.hs
@@ -23,51 +23,60 @@ instance MinMaxExcess1 [Bool] where
     where go minE maxE e (x:xs)                   = let ne = if x then e + 1 else e - 1 in
                                                         go (minE `min` ne) (maxE `max` ne) ne xs
           go minE maxE e _                        = (minE, e, maxE)
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 Word8 where
   minMaxExcess1 = go 0 0 0 0
     where go minE maxE e n w | n < fixedBitSize w = let ne = if w .?. fromIntegral n then e + 1 else e - 1 in
                                                         go (minE `min` ne) (maxE `max` ne) ne (n + 1) w
           go minE maxE e _ _                       = (minE, e, maxE)
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 Word16 where
   minMaxExcess1 = go 0 0 0 0
     where go minE maxE e n w | n < fixedBitSize w = let ne = if w .?. fromIntegral n then e + 1 else e - 1 in
                                                         go (minE `min` ne) (maxE `max` ne) ne (n + 1) w
           go minE maxE e _ _                      = (minE, e, maxE)
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 Word32 where
   minMaxExcess1 = go 0 0 0 0
     where go minE maxE e n w | n < fixedBitSize w = let ne = if w .?. fromIntegral n then e + 1 else e - 1 in
                                                         go (minE `min` ne) (maxE `max` ne) ne (n + 1) w
           go minE maxE e _ _                      = (minE, e, maxE)
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 Word64 where
   minMaxExcess1 = go 0 0 0 0
     where go minE maxE e n w | n < fixedBitSize w = let ne = if w .?. fromIntegral n then e + 1 else e - 1 in
                                                         go (minE `min` ne) (maxE `max` ne) ne (n + 1) w
           go minE maxE e _ _                      = (minE, e, maxE)
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 (DVS.Vector Word8) where
   minMaxExcess1 = DVS.foldl gen (0, 0, 0)
     where gen :: (MinExcess, Excess, MaxExcess) -> Word8 -> (MinExcess, Excess, MaxExcess)
           gen (minE, e, maxE) w = let (wMinE, wE, wMaxE) = minMaxExcess1 w  in
                                   (minE `min` (wMinE + e), e + wE, maxE `max` (wMaxE + e))
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 (DVS.Vector Word16) where
   minMaxExcess1 = DVS.foldl gen (0, 0, 0)
     where gen :: (MinExcess, Excess, MaxExcess) -> Word16 -> (MinExcess, Excess, MaxExcess)
           gen (minE, e, maxE) w = let (wMinE, wE, wMaxE) = minMaxExcess1 w  in
                                   (minE `min` (wMinE + e), e + wE, maxE `max` (wMaxE + e))
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 (DVS.Vector Word32) where
   minMaxExcess1 = DVS.foldl gen (0, 0, 0)
     where gen :: (MinExcess, Excess, MaxExcess) -> Word32 -> (MinExcess, Excess, MaxExcess)
           gen (minE, e, maxE) w = let (wMinE, wE, wMaxE) = minMaxExcess1 w  in
                                   (minE `min` (wMinE + e), e + wE, maxE `max` (wMaxE + e))
+  {-# INLINE minMaxExcess1 #-}
 
 instance MinMaxExcess1 (DVS.Vector Word64) where
   minMaxExcess1 = DVS.foldl gen (0, 0, 0)
     where gen :: (MinExcess, Excess, MaxExcess) -> Word64 -> (MinExcess, Excess, MaxExcess)
           gen (minE, e, maxE) w = let (wMinE, wE, wMaxE) = minMaxExcess1 w  in
                                   (minE `min` (wMinE + e), e + wE, maxE `max` (wMaxE + e))
+  {-# INLINE minMaxExcess1 #-}
