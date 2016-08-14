fdcce64ac0d6343ea0cd4f01fbb190400b3f7806
John Ky <newhoggy@gmail.com>
2016-08-14 23:34:47 +1000

    Simplify

--- src/HaskellWorks/Data/Succinct/BalancedParens/RangeMinMax/L1.hs
+++ src/HaskellWorks/Data/Succinct/BalancedParens/RangeMinMax/L1.hs
@@ -25,10 +25,7 @@ import           HaskellWorks.Data.Succinct.Excess.MinMaxExcess1
 import           HaskellWorks.Data.Vector.VectorLike
 
 data RangeMinMaxL1 = RangeMinMaxL1
-  { rangeMinMaxL1Simple   :: RangeMinMaxSimple
-  , rangeMinMaxL1L0Min    :: DVS.Vector Int8
-  , rangeMinMaxL1L0Max    :: DVS.Vector Int8
-  , rangeMinMaxL1L0Excess :: DVS.Vector Int8
+  { rangeMinMaxL1Base     :: RangeMinMaxL0
   , rangeMinMaxL1Min      :: DVS.Vector Int16
   , rangeMinMaxL1Max      :: DVS.Vector Int16
   , rangeMinMaxL1Excess   :: DVS.Vector Int16
@@ -44,58 +41,45 @@ bitsL1 = fromIntegral (wordsL1 * 64)
 class MkRangeMinMaxL1 a where
   mkRangeMinMaxL1 :: a -> RangeMinMaxL1
 
-instance MkRangeMinMaxL1 RangeMinMaxSimple where
-  mkRangeMinMaxL1 simple = RangeMinMaxL1
-    { rangeMinMaxL1Simple = simple
-    , rangeMinMaxL1L0Min    = DVS.constructN lenL0 (\v -> let (minE, _, _) = allMinMaxL0 DV.! DVS.length v in fromIntegral minE)
-    , rangeMinMaxL1L0Max    = DVS.constructN lenL0 (\v -> let (_, _, maxE) = allMinMaxL0 DV.! DVS.length v in fromIntegral maxE)
-    , rangeMinMaxL1L0Excess = rangeMinMaxL0ExcessA
+instance MkRangeMinMaxL1 RangeMinMaxL0 where
+  mkRangeMinMaxL1 rmmL0 = RangeMinMaxL1
+    { rangeMinMaxL1Base     = rmmL0
     , rangeMinMaxL1Min      = DVS.constructN lenL1 (\v -> let (minE, _, _) = allMinMaxL1 DV.! DVS.length v in fromIntegral minE)
     , rangeMinMaxL1Max      = DVS.constructN lenL1 (\v -> let (_, _, maxE) = allMinMaxL1 DV.! DVS.length v in fromIntegral maxE)
     , rangeMinMaxL1Excess   = rangeMinMaxL1ExcessA
     }
-    where lenSimple     = fromIntegral (vLength (rangeMinMaxSimpleBP simple)) :: Int
-          lenL0         = lenSimple + 1
-          bp            = rangeMinMaxSimpleBP simple
-          allMinMaxL0   = DV.constructN lenL0 genMinMaxL0
-          genMinMaxL0 v = let doneLen = DV.length v in if doneLen == lenSimple
-                            then (0, 0, 0)
-                            else minMaxExcess1 (bp !!! fromIntegral doneLen)
-          lenL1         = fromIntegral (vLength bp) `div` wordsL1 + 1 :: Int
+    where bp            = rangeMinMaxSimpleBP (rangeMinMaxSimple rmmL0)
+          lenL1         = DVS.length (rangeMinMaxL0Min rmmL0) `div` wordsL1 + 1 :: Int
           allMinMaxL1   = DV.constructN lenL1 genMinMaxL1
-          genMinMaxL1 v = let len = DV.length v in
-                          minMaxExcess1 (DVS.take wordsL1 (DVS.drop (fromIntegral len * wordsL1) bp))
-          rangeMinMaxL0ExcessA = DVS.constructN lenL0 (\v -> let (_, e,    _) = allMinMaxL0 DV.! DVS.length v in fromIntegral e) :: DVS.Vector Int8
+          genMinMaxL1 v = let len = DV.length v in minMaxExcess1 (DVS.take wordsL1 (DVS.drop (fromIntegral len * wordsL1) bp))
           rangeMinMaxL1ExcessA = DVS.constructN lenL1 (\v -> let (_, e,    _) = allMinMaxL1 DV.! DVS.length v in fromIntegral e) :: DVS.Vector Int16
+
+instance MkRangeMinMaxL1 RangeMinMaxSimple where
+  mkRangeMinMaxL1 = mkRangeMinMaxL1 . mkRangeMinMaxL0
 
 instance MkRangeMinMaxL1 (DVS.Vector Word64) where
   mkRangeMinMaxL1 = mkRangeMinMaxL1 . mkRangeMinMaxSimple
   {-# INLINE mkRangeMinMaxL1 #-}
 
 instance TestBit RangeMinMaxL1 where
-  (.?.) = (.?.) . rangeMinMaxL1Simple
+  (.?.) = (.?.) . rangeMinMaxL1Base
   {-# INLINE (.?.) #-}
 
 instance Rank1 RangeMinMaxL1 where
-  rank1 = rank1 . rangeMinMaxL1Simple
+  rank1 = rank1 . rangeMinMaxL1Base
   {-# INLINE rank1 #-}
 
 instance Rank0 RangeMinMaxL1 where
-  rank0 = rank0 . rangeMinMaxL1Simple
+  rank0 = rank0 . rangeMinMaxL1Base
   {-# INLINE rank0 #-}
 
 instance BitLength RangeMinMaxL1 where
-  bitLength = bitLength . rangeMinMaxL1Simple
+  bitLength = bitLength . rangeMinMaxL1Base
   {-# INLINE bitLength #-}
 
 instance RangeMinMaxDerived RangeMinMaxL1 where
   type RangeMinMaxBase RangeMinMaxL1 = RangeMinMaxL0
-  rmmBase v = RangeMinMaxL0
-    { rangeMinMaxSimple   = rangeMinMaxL1Simple v
-    , rangeMinMaxL0Min    = rangeMinMaxL1L0Min v
-    , rangeMinMaxL0Max    = rangeMinMaxL1L0Max v
-    , rangeMinMaxL0Excess = rangeMinMaxL1L0Excess v
-    }
+  rmmBase = rangeMinMaxL1Base
   {-# INLINE rmmBase #-}
 
 instance RangeMinMax RangeMinMaxL1 where
@@ -117,11 +101,11 @@ instance RangeMinMax RangeMinMaxL1 where
   {-# INLINE rmmFindCloseN        #-}
 
 instance OpenAt RangeMinMaxL1 where
-  openAt = openAt . rangeMinMaxL1Simple
+  openAt = openAt . rangeMinMaxL1Base
   {-# INLINE openAt #-}
 
 instance CloseAt RangeMinMaxL1 where
-  closeAt = closeAt . rangeMinMaxL1Simple
+  closeAt = closeAt . rangeMinMaxL1Base
   {-# INLINE closeAt #-}
 
 instance BalancedParens RangeMinMaxL1 where
