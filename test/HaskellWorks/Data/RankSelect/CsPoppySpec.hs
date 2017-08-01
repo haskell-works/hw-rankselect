795122a10c85a310ca18b1ec9b19b50552d1e5e6
John Ky <newhoggy@gmail.com>
2017-08-01 22:22:45 +1000

    Upgrade some tests to use hedgehog

--- test/HaskellWorks/Data/RankSelect/CsPoppySpec.hs
+++ test/HaskellWorks/Data/RankSelect/CsPoppySpec.hs
@@ -4,21 +4,27 @@
 
 module HaskellWorks.Data.RankSelect.CsPoppySpec (spec) where
 
-import           GHC.Exts
-import           Data.Maybe
-import qualified Data.Vector.Storable                                       as DVS
-import           Data.Word
-import           HaskellWorks.Data.AtIndex
-import           HaskellWorks.Data.Bits.BitRead
-import           HaskellWorks.Data.Bits.BitShow
-import           HaskellWorks.Data.Bits.PopCount.PopCount1
-import           HaskellWorks.Data.RankSelect.Base.Rank1
-import           HaskellWorks.Data.RankSelect.Base.Select1
-import           HaskellWorks.Data.RankSelect.BasicGen
-import           HaskellWorks.Data.RankSelect.CsPoppy
-import           Prelude hiding (length)
-import           Test.Hspec
-import           Test.QuickCheck
+import Data.Maybe
+import Data.Word
+import GHC.Exts
+import HaskellWorks.Data.AtIndex
+import HaskellWorks.Data.Bits.BitRead
+import HaskellWorks.Data.Bits.BitShow
+import HaskellWorks.Data.Bits.PopCount.PopCount1
+import HaskellWorks.Data.RankSelect.Base.Rank1
+import HaskellWorks.Data.RankSelect.Base.Select1
+import HaskellWorks.Data.RankSelect.BasicGen
+import HaskellWorks.Data.RankSelect.CsPoppy
+import HaskellWorks.Hspec.Hedgehog
+import Hedgehog
+import Prelude                                   hiding (length)
+import Test.Hspec
+-- import Test.QuickCheck
+
+import qualified Data.Vector.Storable      as DVS
+import qualified HaskellWorks.Hedgehog.Gen as G
+import qualified Hedgehog.Gen              as G
+import qualified Hedgehog.Range            as R
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
@@ -28,60 +34,61 @@ newtype ShowVector a = ShowVector a deriving (Eq, BitShow)
 instance BitShow a => Show (ShowVector a) where
   show = bitShow
 
-vectorSizedBetween :: Int -> Int -> Gen (ShowVector (DVS.Vector Word64))
-vectorSizedBetween a b = do
-  n   <- choose (a, b)
-  xs  <- sequence [ arbitrary | _ <- [1 .. n] ]
-  return $ ShowVector (fromList xs)
+-- vectorSizedBetween :: Int -> Int -> Gen (ShowVector (DVS.Vector Word64))
+-- vectorSizedBetween a b = do
+--   n   <- choose (a, b)
+--   xs  <- sequence [ arbitrary | _ <- [1 .. n] ]
+--   return $ ShowVector (fromList xs)
+
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.RankSelect.CsPoppy.Rank1Spec" $ do
   genRank1Select1Spec (undefined :: CsPoppy)
   describe "rank1 for Vector Word64 is equivalent to rank1 for CsPoppy" $ do
-    it "on empty bitvector" $
-      let v = DVS.empty in
-      let w = makeCsPoppy v in
-      let i = 0 in
+    it "on empty bitvector" $ require $ withTests 1 $ property $ do
+      let v = DVS.empty
+      let w = makeCsPoppy v
+      let i = 0
       rank1 v i === rank1 w i
-    it "on one basic block" $
-      forAll (vectorSizedBetween 1 8) $ \(ShowVector v) ->
-      forAll (choose (0, length v * 8)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on one basic block" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 1 8) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (length v * 8))
+      let w = makeCsPoppy v
       rank1 v i === rank1 w i
-    it "on two basic blocks" $
-      forAll (vectorSizedBetween 9 16) $ \(ShowVector v) ->
-      forAll (choose (0, length v * 8)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on two basic blocks" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 9 16) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (length v * 8))
+      let w = makeCsPoppy v
       rank1 v i === rank1 w i
-    it "on three basic blocks" $
-      forAll (vectorSizedBetween 17 24) $ \(ShowVector v) ->
-      forAll (choose (0, length v * 8)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on three basic blocks" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 17 24) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (length v * 8))
+      let w = makeCsPoppy v
       rank1 v i === rank1 w i
   describe "select1 for Vector Word64 is equivalent to select1 for CsPoppy" $ do
-    it "on empty bitvector" $
-      let v = DVS.empty in
-      let w = makeCsPoppy v in
-      let i = 0 in
+    it "on empty bitvector" $ require $ withTests 1 $ property $ do
+      let v = DVS.empty
+      let w = makeCsPoppy v
+      let i = 0
       select1 v i === select1 w i
-    it "on one full zero basic block" $
-      let v = fromList [0, 0, 0, 0, 0, 0, 0, 0] :: DVS.Vector Word64 in
-      let w = makeCsPoppy v in
+    it "on one full zero basic block" $ require $ withTests 1 $ property $ do
+      let v = fromList [0, 0, 0, 0, 0, 0, 0, 0] :: DVS.Vector Word64
+      let w = makeCsPoppy v
       select1 v 0 === select1 w 0
-    it "on one basic block" $
-      forAll (vectorSizedBetween 1 8) $ \(ShowVector v) ->
-      forAll (choose (0, popCount1 v)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on one basic block" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 1 8) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (popCount1 v))
+      let w = makeCsPoppy v
       select1 v i === select1 w i
-    it "on two basic blocks" $
-      forAll (vectorSizedBetween 9 16) $ \(ShowVector v) ->
-      forAll (choose (0, popCount1 v)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on two basic blocks" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 9 16) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (popCount1 v))
+      let w = makeCsPoppy v
       select1 v i === select1 w i
-    it "on three basic blocks" $
-      forAll (vectorSizedBetween 17 24) $ \(ShowVector v) ->
-      forAll (choose (0, popCount1 v)) $ \i ->
-      let w = makeCsPoppy v in
+    it "on three basic blocks" $ require $ property $ do
+      v <- forAll $ G.storableVector (R.linear 17 24) (G.word64 R.constantBounded)
+      i <- forAll $ G.word64 (R.linear 0 (popCount1 v))
+      let w = makeCsPoppy v
       select1 v i === select1 w i
   describe "Rank select over large buffer" $ do
     it "Rank works" $ do
