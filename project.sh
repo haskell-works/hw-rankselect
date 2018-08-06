9a2bfcb7be7143cc30f878bc67ecdb9f1fbc8072
John Ky <newhoggy@gmail.com>
2018-08-07 09:42:36 +1000

    Bump criterion upper bounds

---
+++ project.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+STACK_FLAGS="
+  --flag bits-extra:bmi2
+  --flag hw-rankselect-base:bmi2
+  --flag hw-rankselect:bmi2
+"
+
+case $1 in
+  build)
+    stack build \
+      --test --no-run-tests --bench --no-run-benchmarks \
+      $STACK_FLAGS
+    ;;
+
+  test)
+    stack test \
+      $STACK_FLAGS
+    ;;
+
+  bench)
+    stack bench \
+      $STACK_FLAGS
+    ;;
+
+  repl)
+    stack repl \
+      $STACK_FLAGS
+    ;;
+esac
