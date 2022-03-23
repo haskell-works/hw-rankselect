b387293a98a80aa978c78d5814a20c63f2b1a9e8
John Ky <john.ky@iohk.io>
2022-03-23 22:31:40 +1100

    Support ghc-9.2.2

--- app/App/Commands/Build.hs
+++ app/App/Commands/Build.hs
@@ -9,12 +9,12 @@ module App.Commands.Build
 import Control.Lens
 import Control.Monad
 import Data.Generics.Product.Any
-import Data.List
 import HaskellWorks.Data.FromForeignRegion
 import Options.Applicative
 import System.Directory
 
 import qualified App.Commands.Options.Type                            as Z
+import qualified Data.List                                            as L
 import qualified HaskellWorks.Data.RankSelect.CsPoppy                 as CS
 import qualified HaskellWorks.Data.RankSelect.CsPoppy.Internal.Alpha1 as A1
 import qualified HaskellWorks.Data.RankSelect.Poppy512                as P512
@@ -27,14 +27,14 @@ runBuild :: Z.BuildOptions -> IO ()
 runBuild opts = case opts ^. the @"indexType" of
   Z.CsPoppy  -> do
     entries <- getDirectoryContents "data"
-    let files = ("data/" ++) <$> (".ib" `isSuffixOf`) `filter` entries
+    let files = ("data/" ++) <$> (".ib" `L.isSuffixOf`) `filter` entries
     forM_ files $ \file -> do
       putStrLn $ "Loading cspoppy for " <> file
       CS.CsPoppy !_ !_ (A1.CsPoppyIndex !_ !_) <- mmapFromForeignRegion file
       return ()
   Z.Poppy512 -> do
     entries <- getDirectoryContents "data"
-    let files = ("data/" ++) <$> (".ib" `isSuffixOf`) `filter` entries
+    let files = ("data/" ++) <$> (".ib" `L.isSuffixOf`) `filter` entries
     forM_ files $ \file -> do
       putStrLn $ "Loading cspoppy for " <> file
       P512.Poppy512 !_ !_ <- mmapFromForeignRegion file
